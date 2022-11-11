925e342dce57fc02094b85db53b325d8f0ec5128
Nic Wanavit <nwanavit@gmail.com>
2022-11-11 19:14:56 +0700

    Initial Commit

---
+++ swiftui4Demo/swiftui4DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui4DemoApp.swift
+//  swiftui4Demo
+//
+//  Created by Thanakij Wanavit on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui4DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
